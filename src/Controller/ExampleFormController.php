d7fcc319ac4c00dc8748e4af08c50c7bf372290a
Miary <miary@miary.dev>
2020-01-31 21:30:21 +0100

    Form: handleRequest(Request ) + isSubmitted() + isValid()

--- src/Controller/ExampleFormController.php
+++ src/Controller/ExampleFormController.php
@@ -8,6 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -36,16 +37,24 @@ class ExampleFormController extends AbstractController
     }
 
     /**
-     * @Route("/type", name="simple")
+     * @Route("/type", name="type")
      */
     // http://madatsara.localhost/example/form/type
-    public function type()
+    public function type(Request $request)
     {
         $task = new Task();
         $task->setTask('Write a blog post');
         $task->setDueDate(new \DateTime('tomorrow'));
 
         $form = $this->createForm(TaskType::class, $task);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $data = $form->getData();
+
+            return $this->redirectToRoute('frontend_default');
+        }
 
         return $this->render('example_form/index.html.twig', [
             'form' => $form->createView(),
