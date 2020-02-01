d0af7c9e67e5203a8ac1d554a3d8a695e5f5507a
Miary <miary@miary.dev>
2020-02-01 17:15:37 +0100

    Form: Form Types references

--- src/Controller/ExampleFormController.php
+++ src/Controller/ExampleFormController.php
@@ -5,9 +5,36 @@ namespace App\Controller;
 use App\Entity\Task;
 use App\Form\Type\TaskType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\ColorType;
+use Symfony\Component\Form\Extension\Core\Type\CountryType;
+use Symfony\Component\Form\Extension\Core\Type\CurrencyType;
+use Symfony\Component\Form\Extension\Core\Type\DateIntervalType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\LanguageType;
+use Symfony\Component\Form\Extension\Core\Type\LocaleType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\PercentType;
+use Symfony\Component\Form\Extension\Core\Type\RadioType;
+use Symfony\Component\Form\Extension\Core\Type\RangeType;
+use Symfony\Component\Form\Extension\Core\Type\SearchType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TimeType;
+use Symfony\Component\Form\Extension\Core\Type\TimezoneType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\WeekType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -124,7 +151,7 @@ class ExampleFormController extends AbstractController
      * @Route("/guessing", name="guessing")
      */
     // http://madatsara.localhost/example/form/guessing
-    public function guessing()
+    public function guessing(Request $request)
     {
         $task = new Task();
 
@@ -133,8 +160,119 @@ class ExampleFormController extends AbstractController
             ->add('task')
             ->add('todo', null, ['attr' => ['maxlength' => 6]])
             ->add('dueDate', null)
+            ->add('agreeTermsOfService', CheckboxType::class, ['mapped' => false])
             ->add('save', SubmitType::class, ['label' => 'Add task'])
             ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $tos = $form->get('agreeTermsOfService')->getData();
+
+            return new Response('<body><h1>Agree: ' . $tos . '</h1></body>');
+        }
+
+        return $this->render('example_form/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/typereferences", name="typereferences")
+     */
+    // http://madatsara.localhost/example/form/typereferences
+    public function typereferences(Request $request)
+    {
+
+        $collections = range('a', 'g');
+        $form = $this->createFormBuilder()
+
+            // Text  Fields
+            ->add('TextTypeInheritOptions', TextType::class, [
+                'label' => 'Champ avec Inherit options',
+                'disabled' => true, // disabled attribute
+                'help' => '<b>Texte aide</b>', // Help text
+                'help_html' => true, // allow HTML tag inside help
+                'help_attr' => [ // Help HTML attributes
+                    'class' => 'class-aide'
+                ],
+                'row_attr' => [
+                    'class' => 'group-row-text',
+                    'id' => 'o785emlf'
+                ],
+                'attr' => [
+                    'class' => 'input-text',
+                    'data-id' => 'd-o785emlf'
+                ],
+                'data' => 'Valeur par defaut', // value of field
+                'empty_data' => 'John Doe', // Value saved if field is null or blank
+            ]) // <input type="text" name="form[TextTypeGuessing]">
+
+
+            ->add('TextType', TextType::class) // <input type="text" name="form[TextType]">
+            ->add('TextareaType', TextareaType::class) // <textarea name="form[TextareaType]">
+            ->add('EmailType', EmailType::class) // <input type="email" name="form[EmailType]">
+            ->add('IntegerType', IntegerType::class) // <input type="number" name="form[IntegerType]">
+            ->add('MoneyType', MoneyType::class) // <div><label>Money type</label><div><div class="input-group-prepend"><span class="input-group-text">€ </span></div><input type="text" name="form[MoneyType]" />
+            ->add('NumberType', NumberType::class) // <input type="text" name="form[NumberType]" />
+            ->add('PasswordType', PasswordType::class) // <input type="password" name="form[PasswordType]" />
+            ->add('PercentType', PercentType::class) // <input type="text" name="form[PercentType]" />
+            ->add('SearchType', SearchType::class) // <input type="text" name="form[SearchType]" />
+            ->add('UrlType', UrlType::class) // <input type="text" name="form[UrlType]" inputmode="url" />
+            ->add('RangeType', RangeType::class) // <input type="range" name="form[RangeType]" />
+            ->add('TelType', TelType::class) // <input type="tel" name="form[TelType]" />
+            ->add('ColorType', ColorType::class) // <input type="color" name="form[ColorType]" />
+
+            // Choice Fields
+            ->add('ChoiceType', ChoiceType::class, [
+                'choices' => $collections
+            ]) //
+            ->add('CountryType', CountryType::class) //
+            ->add('LanguageType', LanguageType::class) //
+            ->add('LocaleType', LocaleType::class) //
+            ->add('TimezoneType', TimezoneType::class) //
+            ->add('CurrencyType', CurrencyType::class) //
+
+            // Date and Time Fields
+            ->add('DateType', DateType::class) //
+            ->add('DateIntervalType', DateIntervalType::class) //
+            ->add('DateTimeType', DateTimeType::class) //
+            ->add('TimeType', TimeType::class) //
+            ->add('BirthdayType', BirthdayType::class) //
+            ->add('WeekType', WeekType::class) //
+
+            // Other Fields
+            ->add('CheckboxType', CheckboxType::class)
+            ->add('FileType', FileType::class)
+            ->add('RadioType', ChoiceType::class, [
+                'choices' => [
+                    'May be' => null,
+                    'Yes' => true,
+                    'No' => false
+                ],
+                'expanded' => true
+            ]) // radioType
+
+            // Field groups
+            ->add('collections', CollectionType::class, [
+                'entry_type' => EmailType::class
+            ])
+
+
+            // Button Fields
+            ->add('SubmitType', SubmitType::class)
+
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $tos = $form->get('agreeTermsOfService')->getData();
+
+            return new Response('<body><h1>Agree: ' . $tos . '</h1></body>');
+        }
 
         return $this->render('example_form/index.html.twig', [
             'form' => $form->createView(),
