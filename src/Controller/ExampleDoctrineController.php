904cc82e37faacc6cc751a352e83c8243fd9adfb
Miary <miary@miary.dev>
2020-02-10 21:21:39 +0100

    Doctrine: fetch objects from database - getDoctrine()->getRepository(Api::class)->find(id) -- ApiRepository

--- src/Controller/ExampleDoctrineController.php
+++ src/Controller/ExampleDoctrineController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Api;
+use App\Repository\ApiRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ExampleDoctrineController extends AbstractController
@@ -30,5 +32,29 @@ class ExampleDoctrineController extends AbstractController
         return $this->render('example_doctrine/index.html.twig', [
             'controller_name' => 'ExampleDoctrineController',
         ]);
+    }
+
+    /**
+     * @Route("/example/doctrine/show/{id}", name="example_doctrine_show")
+     */
+    // http://madatsara.localhost/example/doctrine/show/1
+    public function show($id)
+    {
+
+        $api = $this->getDoctrine()->getRepository(Api::class)->find($id);
+
+        return new Response('<p>' . $api->getName() . '</p>');
+    }
+
+    /**
+     * @Route("/example/doctrine/show_other/{id}", name="example_doctrine_show_other")
+     */
+    // http://madatsara.localhost/example/doctrine/show_other/1
+    public function show_other($id, ApiRepository $repository)
+    {
+
+        $api = $repository->find($id);
+
+        return new Response('<p>show_other : ' . $api->getName() . '</p>');
     }
 }
