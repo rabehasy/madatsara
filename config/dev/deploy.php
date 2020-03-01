e1fcfa0d01deac77b0c1cf16d21c7e9110498f4b
Miary <miary@miary.dev>
2020-03-01 23:23:08 +0100

    Easydeploy - dev

---
+++ config/dev/deploy.php
@@ -0,0 +1,40 @@
+<?php
+
+use EasyCorp\Bundle\EasyDeployBundle\Deployer\DefaultDeployer;
+
+return new class extends DefaultDeployer
+{
+    public function configure()
+    {
+        return $this->getConfigBuilder()
+            // SSH connection string to connect to the remote server (format: user@host-or-IP:port-number)
+            ->server('madatsara')
+            // the absolute path of the remote server directory where the project is deployed
+            ->deployDir('/home/web/madatsara/dev')
+            // the URL of the Git repository where the project code is hosted
+            ->repositoryUrl('git@gitlab.com:devmr/madatsarasf5.git')
+            // the repository branch to deploy
+            ->repositoryBranch('master')
+            // avoid error: Script @auto-scripts was called via post-install-cmd
+            ->composerInstallFlags('--prefer-dist --no-interaction --no-dev')
+            // avoid PHP Fatal error:  Uncaught Symfony\Component\Dotenv\Exception\PathException: Unable to read the
+            ->sharedFilesAndDirs(['.env', '.env.local'])
+            // Releases folder to keep
+            ->keepReleases(1)
+            ;
+    }
+
+    public function beforePublishing()
+    {
+        $this->log('<h1>chown -R web: /releases</>');
+        $this->runRemote('sudo chown -R web: {{ deploy_dir }}/releases');
+    }
+    // run some local or remote commands after the deployment is finished
+    public function beforeFinishingDeploy()
+    {
+        // $this->runRemote('{{ console_bin }} app:my-task-name');
+        // $this->runLocal('say "The deployment has finished."');
+        $this->log('<h1>chown -R www-data: /current/var</>');
+        $this->runRemote('sudo chown -R www-data: {{ deploy_dir }}/current/var');
+    }
+};
