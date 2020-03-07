ace7579341656e8f526349429b3fe9a3430aee0a
Miary <miary@miary.dev>
2020-03-07 19:41:55 +0100

    Hamburger.js

--- public/assets/js/javascript.js
+++ public/assets/js/javascript.js
@@ -1,8 +1,28 @@
-var hamburgers = document.querySelectorAll(".hamburger");
-if (hamburgers.length > 0) {
-    hamburgers.forEach(function(hamburger) {
-        hamburger.addEventListener("click", function() {
-            this.classList.toggle("is-active");
-        }, false);
-    });
-}
+
+// Navbar Toggle
+document.addEventListener('DOMContentLoaded', function () {
+
+    // Get all "navbar-burger" elements
+    var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.hamburger'), 0);
+
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length > 0) {
+
+        // Add a click event on each of them
+        $navbarBurgers.forEach(function ($el) {
+            $el.addEventListener('click', function () {
+
+                // Add class
+                this.classList.toggle("is-active");
+
+                // Get the "main-nav" element
+                var $target = document.getElementById('main-nav');
+
+                // Toggle the class on "main-nav"
+                $target.classList.toggle('hidden');
+
+            });
+        });
+    }
+
+});
