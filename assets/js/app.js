5c8d2743af4fc9824cef457c196bc12cb4148319
Miary <miary@miary.dev>
2020-03-08 15:25:50 +0100

    encore - enableSassLoader() + yarn add sass-loader@^7.0.1 node-sass --dev

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,11 +6,10 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import '../css/app.css';
+import '../css/app.scss';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
 
-import greet from './components/greet';
-
-alert(greet('miary'));
+// import greet from './components/greet';
+// alert(greet('miary'));
