62febf1f48b286c10b3c65a0101338af3fd4dc75
Miary <miary@miary.dev>
2020-03-08 15:03:59 +0100

    encore - require module js

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,4 +11,6 @@ import '../css/app.css';
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
 
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+var greet = require('./components/greet');
+
+alert(greet('miary'));
