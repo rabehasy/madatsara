2cd41a0f89ee5470deed09e7b9b9b7f6ee518ea7
Miary <miary@miary.dev>
2020-03-08 17:37:16 +0100

    replace cdn fontawesome --> import '@fortawesome/fontawesome-free/'

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,6 +8,11 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import '../css/app.scss';
 
+import '@fortawesome/fontawesome-free/scss/fontawesome.scss';
+import '@fortawesome/fontawesome-free/scss/regular.scss';
+import '@fortawesome/fontawesome-free/scss/solid.scss';
+import '@fortawesome/fontawesome-free/scss/brands.scss';
+
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
 
