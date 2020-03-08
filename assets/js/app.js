36cbc5ae770d8d9f7e0347aaa90f4f03230e04ac
Miary <miary@miary.dev>
2020-03-08 17:51:35 +0100

    import 'tailwindcss/dist/*'

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,12 +6,21 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import '../css/app.scss';
 
+// Fontawesome
 import '@fortawesome/fontawesome-free/scss/fontawesome.scss';
 import '@fortawesome/fontawesome-free/scss/regular.scss';
 import '@fortawesome/fontawesome-free/scss/solid.scss';
 import '@fortawesome/fontawesome-free/scss/brands.scss';
+
+// Tailwind
+import 'tailwindcss/dist/base.css';
+import 'tailwindcss/dist/components.css';
+import 'tailwindcss/dist/utilities.css';
+
+import '../css/app.scss';
+
+
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
