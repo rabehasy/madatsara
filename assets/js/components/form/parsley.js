01e6a0e102cbd90e3ff86d31a6c07573b0c88c57
Miary <miary@miary.dev>
2020-07-06 23:18:51 +0200

    feat (js): parsley - add submit

--- assets/js/components/form/parsley.js
+++ assets/js/components/form/parsley.js
@@ -6,10 +6,7 @@ $(function () {
         var ok = $('.parsley-error').length === 0;
         $('.bs-callout-info').toggleClass('hidden', !ok);
         $('.bs-callout-warning').toggleClass('hidden', ok);
-    })
-    .on('form:submit', function() {
-        return false; // Don't submit form for this demo
-    });
+    }) ;
 });
 
 
